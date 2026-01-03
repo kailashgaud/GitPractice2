4d430574c5ace8ecfc2ee5ceadc3f50086129869
kailash <bnyymeta@gmail.com>
2026-01-03 22:58:13 +0530

    updating 5th time

--- mnp.py
+++ mnp.py
@@ -2,7 +2,8 @@ Vare=4
 print("Arshita Sallagari 26")
 print("tata Bye  bye jai")
 print("signal")
-print("e meri jinda gi")
+print("e meri jinda gi......")
 vart=3
 print(Vare+vart)
-print("1111")
+print("1111443")
+print("")
