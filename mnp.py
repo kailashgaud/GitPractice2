21bdc68fb593844ea1904e26e31fdda1bec76790
kailash <bnyymeta@gmail.com>
2026-01-03 22:46:43 +0530

    commiting file

--- mnp.py
+++ mnp.py
@@ -1,7 +1,8 @@
-
-print("Arshita Sallagari")
+Vare=4
+print("Arshita Sallagari 24")
 print("i am working at BNP Paribas")
 print("tata Bye  bye jai")
 print("signal")
 print("e meri jinda gi")
-
+vart=3
+print(Vare+vart)
