e987c057c9e33d33a0efb09990f22c2f448cb1e8
kailash <bnyymeta@gmail.com>
2026-01-05 20:45:59 +0530

    adding data and input function

---
+++ inputFunctions.py
@@ -0,0 +1,10 @@
+Name= input("enter your name ")
+print(type(Name))
+age = input("enter your age ")
+print(type(age))
+height = input("enter your height ")
+print(type(height))
+
+BMI=int(age)*float(height)
+print(BMI)
+
